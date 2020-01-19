8930a265c7da878c56af60b37d849dd32e9da69a
Chaitenya Gupta <48187468+Chaitenyag@users.noreply.github.com>
2020-01-19 10:31:01 -0800

    Update message.py

--- message.py
+++ message.py
@@ -13,20 +13,20 @@ from pynput.keyboard import Key, Controller
 
 keyboard = Controller()
 
-account_sid = 'AC190646d0b130afd7dbaebb29f0db6b22'
-auth_token = 'ce5b2e58a960118631b5e9b8240554f7'
+account_sid = 'A____'
+auth_token = 'c______'
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
                               body='I AM JACK I AM IN AN EMERGENCY: {38.520971, -121.748178}',
-                              from_='+16072694132',
-                              to='+14155289715'
+                              from_='+1',
+                              to='+1'
                           )
 TWIML_INSTRUCTIONS_URL = \
 "http://static.fullstackpython.com/phone-calls-python.xml"
 
-TWILIO_PHONE_NUMBER = "+16072694132"
-number = "+16692558370"
+TWILIO_PHONE_NUMBER = "+1"
+number = "+1"
 client.calls.create(to=number, from_=TWILIO_PHONE_NUMBER, url=TWIML_INSTRUCTIONS_URL, method="GET")
 
 print(message.sid)
